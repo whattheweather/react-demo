b01a2a305769821b55bb3d60e6913ce0f5497623
Yx <yangyxspace@gmail.com>
2017-11-27 16:30:40 +0800

    'initial'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div>
+    test
+  </div>
+)
+
+export default App;
