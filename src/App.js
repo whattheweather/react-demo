928e1057026f41295ef09ee4183dd174d1b5c906
Yx <yangyxspace@gmail.com>
2017-11-27 16:58:38 +0800

    '1'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div>
-    test
-  </div>
+  <Grid>
+    <Row>
+      <Col xs={4} md={3} style={{background:'red'}}>fas</Col>
+      <Col xs={16} md={9} style={{background:'blue'}}>fdsa</Col>
+    </Row>
+  </Grid>
 )
 
 export default App;
